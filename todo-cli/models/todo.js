f3092137d1f5eb1bef0aeed8ed55c6b91734c4fc
VishnuReddy25 <118453537+VishnuReddy25@users.noreply.github.com>
2023-12-20 09:35:19 +0530

    Update todo.js

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -1,97 +1,98 @@
-'use strict';
-
-const { Op, Model } = require('sequelize');
+"use strict";
+const { Model, Op } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    static associate(models) {
-      Todo.belongsTo(models.User, {
-        foreignKey: 'userId'
-      });
+    static async addTask(params) {
+      return await Todo.create(params);
     }
 
-    static addTodo({ title, dueDate, userId }) {
-      return Todo.create({ title, dueDate, completed: false, userId });
+    static async showList() {
+      console.log("My Todo list \n");
+
+      console.log("Overdue");
+      console.log(
+        (await Todo.overdue())
+          .map((todo) => {
+            return todo.displayableString();
+          })
+          .join("\n")
+      );
+      console.log("\n");
+
+      console.log("Due Today");
+      console.log(
+        (await Todo.dueToday())
+          .map((todo) => todo.displayableString())
+          .join("\n")
+      );
+      console.log("\n");
+
+      console.log("Due Later");
+      console.log(
+        (await Todo.dueLater())
+          .map((todo) => todo.displayableString())
+          .join("\n")
+      );
     }
 
-    static async overdue(userId) {
+    static async overdue() {
       return await Todo.findAll({
         where: {
-          dueDate: {
-            [Op.lt]: new Date().toLocaleDateString("en-CA"),
-          },
-          userId,
-          completed: false
+          dueDate: { [Op.lt]: new Date().toLocaleDateString("en-CA") },
         },
-        order: [["id", "ASC"]],
       });
     }
 
-    static async dueLater(userId) {
+    static async dueToday() {
       return await Todo.findAll({
         where: {
-          dueDate: {
-            [Op.gt]: new Date(),
+          dueDate: { [Op.eq]: new Date().toLocaleDateString("en-CA") },
+        },
+      });
+    }
+
+    static async dueLater() {
+      return await Todo.findAll({
+        where: {
+          dueDate: { [Op.gt]: new Date().toLocaleDateString("en-CA") },
+        },
+      });
+    }
+
+    static async markAsComplete(id) {
+      await Todo.update(
+        { completed: true },
+        {
+          where: {
+            id: id,
           },
-          userId,
-          completed: false
         }
-      });
+      );
     }
 
-    static dueToday(userId) {
-      return this.findAll({
-        where: {
-          dueDate: {
-            [Op.eq]: new Date().toLocaleDateString("en-CA"),
-          },
-          userId,
-          completed: false
-        },
-        order: [["id", "ASC"]],
-      });
-    }
-
-    static async completedItems(userId) {
-      return await Todo.findAll({
-        where: {
-          completed: true,
-          userId,
-        },
-        order: [["id", "ASC"]],
-      });
-    }
-
-    setCompletionStatus() {
-      if (this.completed === true) {
-        return this.update({ completed: false });
-      } else if (this.completed === false) {
-        return this.update({ completed: true });
+    displayableString() {
+      let checkbox = this.completed ? "[x]" : "[ ]";
+      let today = new Date().toLocaleDateString("en-CA");
+      if (this.dueDate === today) {
+        return `${this.id}. ${checkbox} ${this.title}`;
+      } else {
+        return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
       }
     }
   }
 
-  Todo.init({
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: 5,
-        notNull: true
-      }
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
     },
-    dueDate: {
-      type: DataTypes.DATEONLY,
-      allowNull: false,
-      validate: {
-        notNull: true
-      }
-    },
-    completed: { type: DataTypes.BOOLEAN }
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
 
   return Todo;
 };
