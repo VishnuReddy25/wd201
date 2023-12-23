cdb2d28a86e0f1d13bf205750627dbdd3d138c71
VishnuReddy25 <118453537+VishnuReddy25@users.noreply.github.com>
2023-12-23 06:25:49 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,49 +1,45 @@
 class TodoList {
- constructor() {
-   this.allItems = [];
- }
+  constructor() {
+    this.allItems = [];
+  }
 
- addItem(todoItem) {
-   this.allItems.push(todoItem);
- }
+  addItem(todoItem) {
+    this.allItems.push(todoItem);
+  }
 
- markAsComplete(index) {
-   this.allItems[index].completed = true;
- }
+  markAsComplete(index) {
+    this.allItems[index].completed = true;
+  }
 
- getOverdueItems() {
-   const today = new Date().toISOString().split("T")[0];
-   return this.allItems.filter(
-     (item) => !item.completed && item.dueDate < today
-   );
- }
+  getOverdueItems() {
+    const today = new Date().toISOString().split("T")[0];
+    return this.allItems.filter((item) => !item.completed && item.dueDate < today);
+  }
 
- getItemsDueToday() {
-   const today = new Date().toISOString().split("T")[0];
-   return this.allItems.filter((item) => item.dueDate === today);
- }
+  getItemsDueToday() {
+    const today = new Date().toISOString().split("T")[0];
+    return this.allItems.filter((item) => item.dueDate === today);
+  }
 
- getItemsDueLater() {
-   const today = new Date().toISOString().split("T")[0];
-   return this.allItems.filter(
-     (item) => !item.completed && item.dueDate > today
-   );
- }
+  getItemsDueLater() {
+    const today = new Date().toISOString().split("T")[0];
+    return this.allItems.filter((item) => !item.completed && item.dueDate > today);
+  }
 
- getDisplayableList(list) {
-   const today = new Date().toISOString().split("T")[0];
-   return list
-     .map((item, index) => {
-       const dueText = item.dueDate
-         ? item.dueDate === today
-           ? ""
-           : ` <span class="math-inline">\{item\.dueDate\}\`
-\: "";
-const completionStatus \= item\.completed ? "\[x\]" \: "\[ \]";
-return \`</span>{completionStatus} <span class="math-inline">\{item\.title\}</span>{dueText}`;
-     })
-     .join("\n");
- }
+  getDisplayableList(list) {
+    const today = new Date().toISOString().split("T")[0];
+    return list
+      .map((item, index) => {
+        const dueText = item.dueDate
+          ? item.dueDate === today
+            ? ""
+            : ` <span class="math-inline">\{item.dueDate\}` // Fixed missing backslash
+          : "";
+        const completionStatus = item.completed ? "[x]" : "[ ]"; // Removed extra backslashes
+        return `</span>${completionStatus} <span class="math-inline">${item.title}</span>${dueText}`; // Fixed template literal
+      })
+      .join("\n");
+  }
 }
 
 module.exports = TodoList;
